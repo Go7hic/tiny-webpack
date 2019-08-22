642fc1b6dbe1b5289a984178760a36af79bf4ada
Go7hic <gtfx0209@qq.com>
2019-08-22 18:54:01 +0800

    chore: update package

--- template/react-typescript-library/rollup.config.js
+++ template/react-typescript-library/rollup.config.js
@@ -1,13 +1,13 @@
 import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
+import commonjs from '../react-library/node_modules/rollup-plugin-commonjs'
+import external from '../react-library/node_modules/rollup-plugin-peer-deps-external'
 // import postcss from 'rollup-plugin-postcss-modules'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
+import resolve from '../react-library/node_modules/rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import svgr from '../react-library/node_modules/@svgr/rollup'
 
-import pkg from './package.json'
+import pkg from './package.json.js'
 
 export default {
   input: 'src/index.tsx',
